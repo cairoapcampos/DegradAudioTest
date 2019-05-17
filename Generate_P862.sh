3ccab0dc5b243e142d1df067b98a1a5423b50969
Cairo Ap. Campos <cairoapcampos@hotmail.com>
2019-05-17 15:42:10 -0300

    Update Generate_P862.sh

--- Generate_P862.sh
+++ Generate_P862.sh
@@ -2,7 +2,7 @@
 
 for i in $(seq 0 30);
  do
- ./PESQ.862 +16000 m_25_en_c_se01.wav m_25_en_c_se01_Degradado$i.wav
+ ./P862 +16000 m_25_en_c_se01.wav m_25_en_c_se01_Degradado$i.wav
  done
 
-mv pesq_results.txt Result_PESQ.862.txt
+mv pesq_results.txt Result_P862.txt
