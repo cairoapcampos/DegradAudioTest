fa495ddb562e052ccc5bc993e9e81100ef18f7e6
Cairo Ap. Campos <cairoapcampos@hotmail.com>
2019-05-18 23:36:29 -0300

    Update Generate_Report_P563.sh

--- Generate_Report_P563.sh
+++ Generate_Report_P563.sh
@@ -3,4 +3,8 @@
 for i in $(seq 0 30);
  do
   ./P563 m_25_en_c_se01_Degradado$i.wav
- done | tee Result_P563.csv
+ done | tee Result_tmp.txt
+ 
+ 
+echo "Filename        MOS" > Result_P563.txt
+cat Result_tmp.txt | grep -v Filename >> Result_P563.txt
